32d8d6e1aa5a6df59dbc6c984645947040a4a744
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-01-04 15:08:31 +0100

    adding AboutAuthor component

---
+++ src/AboutAuthor/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ContentContainer } from "./styled";
+
+const AboutAuthor = () => (
+    <ContentContainer>
+        <div></div>
+    </ContentContainer>
+);
+
+export default AboutAuthor;
