5c91aa0558acd8c7da388009b795194603ace741
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-01-28 15:33:18 +0100

    adding theme font colors

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,8 @@
 export const theme = {
     color: {
         mainFont: "#252525",
+        secondaryFont: "#6E7E91",
+        thirdFont: "#fff",
         background: "#E5E5E5"
     },
     breakpoint: {
