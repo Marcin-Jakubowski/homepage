90eaf4dcfc7602bb43a9938e3ca7d0a19a8cf3c2
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-01-29 11:44:33 +0100

    adding GlobalStyle to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutAuthor from "./AboutAuthor/index";
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
 import Container from "./common/Container/index";
 import { theme } from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme = {theme}>
+      <GlobalStyle />
       <Container>
         <AboutAuthor />
       </Container>
