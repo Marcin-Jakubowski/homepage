990acd58961a7c123df41b1f59e24ddf5e5ddc64
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-01 15:22:21 +0100

    adding Section component to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import Container from "./common/Container/index";
 import { theme } from "./theme";
+import Section from "./common/Section";
 
 function App() {
+  const bodyTest = [1, 2, 3];
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
         <AboutAuthor />
+        <Section
+          header={"My skillset includes"}
+          body={bodyTest}
+        />
       </Container>
     </ThemeProvider>
   );
