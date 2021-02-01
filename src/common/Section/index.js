61b3f7444d8cd9d127d792509225ed606a353734
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-01 13:46:56 +0100

    adding basic elements to Section component

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Container, HeaderContent } from "./styled";
 
-const Section = () => (
-    <></>
+const Section = (header, body) => (
+    <Container>
+        <HeaderContent>
+            {header}
+        </HeaderContent>
+        <ItemsList>
+            {body}
+        </ItemsList>
+    </Container>
 );
 
 export default Section;
---
+++ src/common/Section/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    padding: 32px;
+`;
+
+export const HeaderContent = styled.p`
+    font-size: 30px;
+    line-height: 36px;
+    font-weight: 900;
+`;
+
+export const ItemsList = styled.div`
+    font-size: 18px;
+    line-height: 25px;
+    font-weight: 400;
+`;
