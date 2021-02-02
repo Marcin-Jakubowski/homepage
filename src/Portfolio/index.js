001d2dafd2bb620bf9e7590d59a2912e597451a9
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-02 15:55:28 +0100

    adding and styling basic Portolio items

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Container, Logo } from "./styled";
+import { Container, Header, Logo, SubHeader } from "./styled";
+import portfolioLogo from "./portfolioLogo.svg";
 
 const Portfolio = () => (
 <Container>
-    
+    <Logo src={portfolioLogo} alt="Portfolio Logo" />
+    <Header>portfolio</Header>
+    <SubHeader>My recent projects</SubHeader>
 </Container>
 );
 
--- src/Portfolio/styled.js
+++ src/Portfolio/styled.js
@@ -6,3 +6,25 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
 `;
+
+export const Logo = styled.img`
+    display: block;
+    min-width: 40px;
+    height: 40px;
+    margin-bottom: 13px;
+`;
+
+export const Header = styled.h2`
+    margin: 0;
+    font-size: 30px;
+    line-height: 36px;
+    font-weight: 900;
+    text-transform: capitalize;
+`;
+
+export const SubHeader = styled.p`
+    margin: 0;
+    font-size: 20px;
+    line-height: 28px;
+    font-weight: 400;
+`;
