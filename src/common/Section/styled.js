4d7ca5760963efc5bc2eb2597d6d70a982b0f1d4
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-02 13:43:10 +0100

    adding margin to Section component

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   padding: 32px;
+  margin-bottom: 72px;
   background-color: ${({ theme }) => theme.color.secondaryBackground};
   border-radius: 4px;
 `;
