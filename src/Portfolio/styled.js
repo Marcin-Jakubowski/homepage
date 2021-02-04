3d76bc5832b10209fc64071a66938e871d5a1846
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-04 16:19:39 +0100

    styling project tile items

--- src/Portfolio/styled.js
+++ src/Portfolio/styled.js
@@ -33,6 +33,7 @@ export const ProjectsContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 32px;
+    margin: 24px auto;
 `;
 
 export const ProjectTile = styled.div`
@@ -42,4 +43,23 @@ export const ProjectTile = styled.div`
     border: 6px solid rgba(209, 213, 218, 0.3);
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
     border-radius: 4px;
+    padding: 56px;
+`;
+
+export const Title = styled.h3`
+    color: ${({theme}) => theme.color.mainBlue};
+    font-size: 24px;
+    line-height: 29px;
+    font-weight: 700;
+    margin: 0;
+`;
+
+export const Content = styled.p`
+    font-size: 18px;
+    line-height: 25px;
+    color: ${({theme}) => theme.color.secondaryFont};
+`;
+
+export const Link = styled.a`
+    color: ${({theme}) => theme.color.mainBlue}
 `;
