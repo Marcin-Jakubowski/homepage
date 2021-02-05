93bb93219cd0f804075d9d39c7bd2bf808e2e03e
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-05 15:39:15 +0100

    adding basic Footer items

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Container } from "./styled";
+import { Container, Content, Mail, SmallContent } from "./styled";
 
 const Footer = () => (
-<Container></Container>
+<Container>
+    <SmallContent>let's talk!</SmallContent>
+    <Mail>marcin.jakubowski137@gmail.com</Mail>
+    <Content>description</Content>
+</Container>
 );
 
 export default Footer;
