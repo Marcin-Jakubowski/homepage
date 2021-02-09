dbb2e45c4a49527732c99e667e12ceab9475cf42
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-09 16:31:28 +0100

    adding box with logos to Footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Container, Content, Mail, SmallContent } from "./styled";
+import { Container, Content, Logo, LogoLink, LogosBox, Mail, SmallContent } from "./styled";
+import facebookLogo from "./facebookLogo.svg";
+import githubLogo from "./githubLogo.svg";
+import linkedinLogo from "./linkedinLogo.svg";
 
 const Footer = () => (
   <Container>
     <SmallContent>let's talk!</SmallContent>
     <Mail>marcin.jakubowski137@gmail.com</Mail>
     <Content>description</Content>
+    <LogosBox>
+      <LogoLink><Logo src={githubLogo} alt= "GitHub Logo" /></LogoLink>
+      <LogoLink><Logo src={facebookLogo} alt= "Facebook Logo" /></LogoLink>
+      <LogoLink><Logo src={linkedinLogo} alt= "LinkedIn Logo" /></LogoLink>
+    </LogosBox>
   </Container>
 );
 
