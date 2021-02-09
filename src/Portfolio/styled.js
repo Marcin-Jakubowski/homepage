7d51d0a4b73215be90a55debc5e37013eea41e57
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-09 15:18:39 +0100

    adding mail box styles

--- src/Portfolio/styled.js
+++ src/Portfolio/styled.js
@@ -64,3 +64,33 @@ export const Content = styled.p`
 export const Link = styled.a`
   color: ${({ theme }) => theme.color.mainBlue};
 `;
+
+export const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const SmallContent = styled.p`
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.color.secondaryFont};
+  margin: 32px 0 12px;
+`;
+
+export const Mail = styled.address`
+  font-size: 32px;
+  line-height: 39px;
+  font-weight: 900;
+  margin: 0;
+  font-style: normal;
+`;
+
+export const Description = styled.p`
+  font-size: 18px;
+  line-height: 25px;
+  font-weight: 400;
+  margin: 32px 0;
+`;
