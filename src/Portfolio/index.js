8eee3a9ac98a1f3f52a033872aa26e176b297d6b
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-10 17:41:52 +0100

    importing initial fetch function to Portfolio

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Container,
   Content,
@@ -10,29 +10,40 @@ import {
   SubHeader,
   Title,
 } from "./styled";
+import { initialFetchAPI } from '../PortfolioSlice';
 import portfolioLogo from "./portfolioLogo.svg";
+import { useDispatch } from "react-redux";
 
-const Portfolio = () => (
-  <Container>
-    <Logo src={portfolioLogo} alt="Portfolio Logo" />
-    <Header>portfolio</Header>
-    <SubHeader>My recent projects</SubHeader>
-    <ProjectsContainer>
-      <ProjectTile>
-        <Title>Project</Title>
-        <Content>description content</Content>
-        <Content>
-          Demo: <Link>link.com</Link>
-        </Content>
-        <Content>
-          Code: <Link>link.com</Link>
-        </Content>
-      </ProjectTile>
-      <ProjectTile></ProjectTile>
-      <ProjectTile></ProjectTile>
-      <ProjectTile></ProjectTile>
-    </ProjectsContainer>
-  </Container>
-);
+function Portfolio() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initialFetchAPI());
+  },
+    [dispatch]);
+
+  return (
+    <Container>
+      <Logo src={portfolioLogo} alt="Portfolio Logo" />
+      <Header>portfolio</Header>
+      <SubHeader>My recent projects</SubHeader>
+      <ProjectsContainer>
+        <ProjectTile>
+          <Title>Project</Title>
+          <Content>description content</Content>
+          <Content>
+            Demo: <Link>link.com</Link>
+          </Content>
+          <Content>
+            Code: <Link>link.com</Link>
+          </Content>
+        </ProjectTile>
+        <ProjectTile></ProjectTile>
+        <ProjectTile></ProjectTile>
+        <ProjectTile></ProjectTile>
+      </ProjectsContainer>
+    </Container>
+  );
+}
 
 export default Portfolio;
