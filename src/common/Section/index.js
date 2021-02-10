669115e36fa4b8d26c6ce620710f4f8eb898559f
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-10 18:19:33 +0100

    adding nanoid unique key

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, HeaderContent, ItemsList, ListItem } from "./styled";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Section = ({ header, body }) => (
   <Container>
     <HeaderContent>{header}</HeaderContent>
     <ItemsList>
-      {body && body.map((fragment) => <ListItem>{fragment}</ListItem>)}
+      {body && body.map((fragment) => <ListItem key={nanoid()}>{fragment}</ListItem>)}
     </ItemsList>
   </Container>
 );
