89676772fb6a5ced159715f2f2e1873473c1f430
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-13 22:52:30 +0100

    code formatting

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Content, Logo, LogoLink, LogosBox, Mail, SmallContent } from "./styled";
+import {
+  Container,
+  Content,
+  Logo,
+  LogoLink,
+  LogosBox,
+  Mail,
+  SmallContent,
+} from "./styled";
 import facebookLogo from "./facebookLogo.svg";
 import githubLogo from "./githubLogo.svg";
 import linkedinLogo from "./linkedinLogo.svg";
@@ -10,9 +18,15 @@ const Footer = () => (
     <Mail>marcin.jakubowski137@gmail.com</Mail>
     <Content>description</Content>
     <LogosBox>
-      <LogoLink><Logo src={githubLogo} alt= "GitHub Logo" /></LogoLink>
-      <LogoLink><Logo src={facebookLogo} alt= "Facebook Logo" /></LogoLink>
-      <LogoLink><Logo src={linkedinLogo} alt= "LinkedIn Logo" /></LogoLink>
+      <LogoLink>
+        <Logo src={githubLogo} alt="GitHub Logo" />
+      </LogoLink>
+      <LogoLink>
+        <Logo src={facebookLogo} alt="Facebook Logo" />
+      </LogoLink>
+      <LogoLink>
+        <Logo src={linkedinLogo} alt="LinkedIn Logo" />
+      </LogoLink>
     </LogosBox>
   </Container>
 );
