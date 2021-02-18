25c5c92cfc155c03894e8838c1b708f727be5eb3
Marcin-Jakubowski <marcin.jakubowski137@gmail.com>
2021-02-18 20:59:09 +0100

    importing mail address from personal info file

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -10,12 +10,13 @@ import {
 } from "./styled";
 import facebookLogo from "./facebookLogo.svg";
 import githubLogo from "./githubLogo.svg";
-import linkedinLogo from "./linkedinLogo.svg";
+// import linkedinLogo from "./linkedinLogo.svg";
+import { mail } from "../personalInfo";
 
 const Footer = () => (
   <Container>
     <SmallContent>let's talk!</SmallContent>
-    <Mail>marcin.jakubowski137@gmail.com</Mail>
+    <Mail href={`mailto:${mail}`}>{mail}</Mail>
     <Content>I’m always open to new projects whenever I have the time. If you need some help to make your ideas come to life or want to start new project, feel free to conatct me 🖐</Content>
     <LogosBox>
       <LogoLink href="https://github.com/Marcin-Jakubowski">
